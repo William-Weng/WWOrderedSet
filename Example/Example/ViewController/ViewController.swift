f22133bc2f9c568ab91737ae4f1614398c758ef2
William.Weng <William.Weng@iit.com.hk>
2025-10-22 14:46:37 +0800

    - update Example

---
+++ Example/Example/ViewController/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2025/10/22.
+//
+
+import UIKit
+import WWOrderedSet
+
+final class ViewController: UIViewController {
+    
+    private let orderedSet = WWOrderedSet<String>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        orderedSet.add("386")
+        orderedSet.add("156")
+        orderedSet.add("251")
+        
+        orderedSet.add("251")
+        orderedSet.add("156")
+        orderedSet.add("386")
+        
+        print(orderedSet)
+        
+        orderedSet.remove("251")
+        
+        print(orderedSet.array)
+        print(orderedSet.contains("386"))
+    }
+}
